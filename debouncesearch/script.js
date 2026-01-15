952ec1b0f4c6c22ea672208e950987cf045fd197
sonamkumari1 <sonamkumari63928@gmail.com>
2026-01-15 15:03:11 +0530

    formvalidation and modal and ratelimiter

--- debouncesearch/script.js
+++ debouncesearch/script.js
@@ -1,27 +1,40 @@
+const searchInput = document.getElementById("search");
+const results = document.getElementById("results");
 
-const searchInput=document.getElementById("search")
-const results=document.getElementById("results")
+function debounce(fn, delay) {
+  let timer; // closure variable
 
-function debounce(fn, delay){
-    let timer;
+  return function (...args) {
+    clearTimeout(timer);
 
-    return function(...args){
-        clearTimeout(timer)
-
-        timer=setTimeout(()=>{
-            fn.apply(this, args)
-        }, delay)
-    }
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
 }
 
-async function fetchUsers(query){
-    if(!query){
-        results.innerHTML=""
-        return;
-    }
+async function fetchUsers(query) {
+  if (!query) {
+    results.innerHTML = "";
+    return;
+  }
 
-    const res=await fetch(`https://jsonplaceholder.typicode.com/users`)
-    const data=res.json()
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users`
+  );
+  const data = await res.json();
 
-    const filtered=data.filter(user=>user.name.toLowercase().includes(query.toLowercase()))
+  const filtered = data.filter(user =>
+    user.name.toLowerCase().includes(query.toLowerCase())
+  );
+
+  results.innerHTML = filtered
+    .map(user => `<li>${user.name}</li>`)
+    .join("");
 }
+
+const debouncedSearch = debounce(fetchUsers, 500);
+
+searchInput.addEventListener("input", (e) => {
+  debouncedSearch(e.target.value);
+});
